b44435eb2ac329de475651d0ff12a0c6b4328b92
Suraj Prajapat <>
2024-09-01 14:20:27 +0530

    FLOAT-002: Commands Setup For Maintaining component information

--- core/framework/setup/readDbSchemas.js
+++ core/framework/setup/readDbSchemas.js
@@ -2,23 +2,36 @@ const db = require('../database/createTables');
 const fs = require('fs');
 const path = require('path');
 const appRoot = require('app-root-path');
+const {validateSchemas} = require('../validator/SchemaValidator');
 
 const coreDir = path.join(appRoot.path, 'core', 'components');
+const schemaDir = path.join(appRoot.path, 'core', 'framework', 'schemas');
 
 const readXmlFiles = async (sequelize, dirPath) => {
+    console.log('xml reading');
     const components = fs.readdirSync(dirPath);
 
     for (const component of components) {
-        const schemaDir = path.join(dirPath, component, 'schema');
-        if (fs.existsSync(schemaDir)) {
-            const xmlFiles = fs.readdirSync(schemaDir).filter(file => file.endsWith('.xml'));
 
+        
+        // Database Setup
+        const xmldDir = path.join(dirPath, component, 'schema');
+        if (fs.existsSync(xmldDir)) {
+            // create & update databases
+            const xmlFiles = fs.readdirSync(xmldDir).filter(file => file.endsWith('.xml'));
+            const dbXsdFile = path.join(schemaDir, 'database.xsd'); 
+            //fs.readdirSync(schemaDir).filter(file => );
+            console.log('dbXsdFile', dbXsdFile);
             for (const file of xmlFiles) {
-                const xmlFilePath = path.join(schemaDir, file);
-                // const xmlFilePath = path.join(__dirname, 'schema.xml');
-                // const xmlFilePath = appRoot.resolve('core/components/catalog/database.xml');
-                const xmlContent = fs.readFileSync(xmlFilePath, 'utf8');
-                db(sequelize, xmlContent);
+                const xmlFilePath = path.join(xmldDir, file);
+                if(validateSchemas(dbXsdFile, xmlFilePath)) {
+                    console.log('create db tables')
+                    // const xmlFilePath = path.join(__dirname, 'schema.xml');
+                    // const xmlFilePath = appRoot.resolve('core/components/catalog/database.xml');
+                    const xmlContent = fs.readFileSync(xmlFilePath, 'utf8');
+                    db(sequelize, xmlContent);
+                }
+               
             }
         }
     }
